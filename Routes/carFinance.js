10eea54b584e69a02a6856255e3b03afb16e810e
johnBWeiss <yoniw7@gmail.com>
2022-04-30 23:45:43 +0300

    extracted price

--- Routes/carFinance.js
+++ Routes/carFinance.js
@@ -3,34 +3,57 @@ const router = express.Router();
 const axios = require("axios");
 const cheerio = require('cheerio');
 
-
-
+const title = []
+const value = []
+const result = []
+const addedDetails = []
+const items = {};
+const itemArray = []
+//https://www.icar.co.il/%D7%9E%D7%97%D7%99%D7%A8%D7%95%D7%9F_%D7%A8%D7%9B%D7%91/%D7%9E%D7%97%D7%99%D7%A8%D7%95%D7%9F_%D7%A7%D7%99%D7%94_%D7%A1%D7%99%D7%93_%D7%A1%D7%98%D7%99%D7%99%D7%A9%D7%9F_sw/
+//https://www.icar.co.il/%D7%9E%D7%97%D7%99%D7%A8%D7%95%D7%9F_%D7%A8%D7%9B%D7%91/%D7%9E%D7%97%D7%99%D7%A8%D7%95%D7%9F_%D7%A1%D7%95%D7%96%D7%95%D7%A7%D7%99_%D7%A7%D7%A8%D7%95%D7%A1%D7%90%D7%95%D7%91%D7%A8/
 router.get("/:carNumber", (req, res) => {
+    axios("https://www.icar.co.il/%D7%9E%D7%97%D7%99%D7%A8%D7%95%D7%9F_%D7%A8%D7%9B%D7%91/%D7%9E%D7%97%D7%99%D7%A8%D7%95%D7%9F_%D7%A1%D7%95%D7%96%D7%95%D7%A7%D7%99_%D7%A7%D7%A8%D7%95%D7%A1%D7%90%D7%95%D7%91%D7%A8/")
+        .then(//https://pricelist.yad2.co.il/search.php?CarType=1&Manufactur=332&CarModel=7400&Year=2021
+            response => {
+                let priceData = []
+                let resp = response.data
+                let $ = cheerio.load(resp)
+                let check = $('.pl_versions_td', resp).text()
+                check = check.split(" ")
+                console.log(check[0]);
+
+                // let check = $('#fieldprice', resp).text()
+                // console.log(typeof (check));
+                // for (let i = 0; i < check.length; i++) {
+                //     console.log(typeof (Number(check.charAt(i))));
+                //     if (!isNaN(Number(check.charAt(i)))) { items["מחיר"] += check.charAt(i) }
+                // }
+                // console.log(items["מחיר"]);
+                // items[priceData[check]] = check
+                // extracting price
+                // console.log(items);
+                // let content = check.toString().replace(/\t/g, '').replace(/\r/g, '').replace(/\n/g, '').replace('מחיר', '').trim()
+                items["מחיר"] = check[0]
+
+
+            })
+
+
+
     axios(`https://meshumeshet.com/c/${req.params.carNumber}`).then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        const title = []
-        const value = []
-        const result = []
-        const addedDetails = []
-        // const engineAndGear = {}
-        const items = {};
-        const itemArray = []
+
 
         $('.carPageShowMoreDetails', html).each(function () {
             const testData = $(this).text().split(":")
             addedDetails.push(testData)
-            console.log(testData, 'testdata');
+            // console.log(testData, 'testdata');
         })
 
-        // engineAndGear[addedDetails[0][0]] = addedDetails[0][1]
-        // console.log(engineAndGear);
-        console.log(addedDetails, 'addeddetails')
-
         items[addedDetails[0][0]] = addedDetails[0][1] // extracting engine gas type
-        items[addedDetails[1][0]] = addedDetails[1][1] // extracting current ownership
-
         items[addedDetails[1][0]] = addedDetails[1][1] // extracting gear type
+
 
         $('dt', html).each(function () {
             const titleData = $(this).text().trim()
