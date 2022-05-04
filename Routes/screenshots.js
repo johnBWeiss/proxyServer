dfa0956d3eb790feefe8c69e7b8cf15f3d91d848
johnBWeiss <yoniw7@gmail.com>
2022-05-04 10:20:05 +0300

    12 sec - now trying to send binary

--- Routes/screenshots.js
+++ Routes/screenshots.js
@@ -42,8 +42,8 @@ const minimal_args = [
     '--use-gl=swiftshader',
     '--use-mock-keychain',
 ];
-// console.time('multi')
-// let testArray = multiple([{ link: 'https://www.google.com/' }, { link: 'https://www.google.com/' }, { link: 'https://www.google.com/', }])
+console.time('multi')
+// let testArray = multiple([{ link: 'https://www.google.com/' }, { link: 'https://www.walla.co.il/' }, { link: 'https://www.ynet.co.il/home/0,7340,L-8,00.html', }])
 // if (testArray.length > 1) {
 //     console.log(testArray);
 
@@ -51,19 +51,25 @@ const minimal_args = [
 // }
 
 async function multiple(searchResults) {
+
     console.time('multiple')
-    let image1 = screenShot(searchResults[0].link)
-    let image2 = screenSho(searchResults[1].link)
-    let image3 = screenSh(searchResults[2].link)
-    if (image1.length > 0 && image2.length > 0 && image3.length > 0) {
-        image1 = 'data:image/png;base64,' + image1
-        image2 = 'data:image/png;base64,' + image2
-        image3 = 'data:image/png;base64,' + image3
+    try {
+        let image1 = screenShot(searchResults[0].link)
+        let image2 = screenSho(searchResults[1].link)
+        let image3 = screenSh(searchResults[2].link)
+        if (image1.length > 0 && image2.length > 0 && image3.length > 0) {
+            image1 = 'data:image/png;base64,' + image1
+            image2 = 'data:image/png;base64,' + image2
+            image3 = 'data:image/png;base64,' + image3
 
 
-        console.timeEnd('multiple')
-        return [image1, image2, image3]
+            console.timeEnd('multiple')
+            return [image1, image2, image3]
+        }
+    } catch (error) {
+        console.log(error);
     }
+
 }
 // cyprus
 // console.time('check')
@@ -82,7 +88,7 @@ async function screenShot(searchResults) {
     const browser = await puppeteer.launch({ headless: true, args: minimal_args });
     const page = await browser.newPage();
     await page.goto(searchResults);
-    let image = await page.screenshot({ type: 'jpeg', quality: 5, omitBackground: true, encoding: 'base64' });
+    let image = await page.screenshot({ type: 'jpeg', quality: 20, omitBackground: true, encoding: 'base64' });
     image = 'data:image/jpeg;base64,' + image
 
     await browser.close();
@@ -98,7 +104,7 @@ async function screenSho(searchResults) {
     const browser = await puppeteer.launch({ headless: true, args: minimal_args });
     const page = await browser.newPage();
     await page.goto(searchResults);
-    let image = await page.screenshot({ type: 'jpeg', quality: 50, omitBackground: true, encoding: 'base64' });
+    let image = await page.screenshot({ type: 'jpeg', quality: 20, omitBackground: true, encoding: 'base64' });
     image = 'data:image/jpeg;base64,' + image
 
     await browser.close();
@@ -116,8 +122,7 @@ async function screenSh(searchResults) {
     const browser = await puppeteer.launch({ headless: true, args: minimal_args });
     const page = await browser.newPage();
     await page.goto(searchResults);
-    let image = await page.screenshot({ type: 'jpeg', quality: 50, omitBackground: true, encoding: 'base64' });
-    image.toString('base64')
+    let image = await page.screenshot({ type: 'jpeg', quality: 20, omitBackground: true, encoding: 'base64' });
     image = 'data:image/jpeg;base64,' + image
 
     await browser.close();
@@ -156,70 +161,79 @@ async function screenSh(searchResults) {
 
 router.get('/google/:searchWord', async (req, res) => {
 
-    console.time('start')
-    console.time('getting links')
-    let browser;
-    const searchQuery = req.params.searchWord;
 
-    browser = await puppeteer.launch({ headless: true, args: minimal_args, userDataDir: './dataCach' });
-    const [page] = await browser.pages();
-    await page.goto("https://www.google.com/", { waitUntil: "domcontentloaded" });
-    await page.waitForSelector('input[aria-label="Search"]', { visible: true });
-    await page.type('input[aria-label="Search"]', searchQuery);
-    await Promise.all([
-        page.waitForNavigation(),
-        page.keyboard.press("Enter"),
-    ]);
-    await page.waitForSelector(".LC20lb", { visible: true });
-    const searchResults = await page.$$eval(".LC20lb", els =>
-        els.map(e => ({ title: e.innerText, link: e.parentNode.href, imageCode: '' }))
-    );
-    console.timeEnd('getting links')
+    try {
+        console.time('start')
+        let browser;
+        const searchQuery = req.params.searchWord;
+        browser = await puppeteer.launch({ headless: true, args: minimal_args, userDataDir: './dataCach' });
+        const [page] = await browser.pages();
+        await page.goto("https://www.google.com/", { waitUntil: "domcontentloaded" });
+        await page.waitForSelector('input[aria-label="Search"]', { visible: true });
+        await page.type('input[aria-label="Search"]', searchQuery);
+        await Promise.all([
+            page.waitForNavigation(),
+            page.keyboard.press("Enter"),
+        ]);
+        console.time('getting links')
 
-    browser?.close();
+        await page.waitForSelector(".LC20lb", { visible: true });
+        const searchResults = await page.$$eval(".LC20lb", els =>
+            els.map(e => ({ title: e.innerText, link: e.parentNode.href, imageCode: '' }))
+        );
+        console.timeEnd('getting links')
 
-    console.log(searchResults);
-    const imagesMultiple = []
-    // [screenShot(searchResults[0]), screenShot(searchResults[1]), screenShot(searchResults[3])]
+        browser?.close();
 
-    // await screenShot(searchResults[0].link).then(await screenShot(searchResults[1].link)).then(await screenShot(searchResults[2].link))
+        // console.log(searchResults);
 
 
-    // imagesMultiple[1] = await screenShot(searchResults[1].link)
-    // imagesMultiple[2] = await screenShot(searchResults[2].link)
+        const imagesMultiple = []
+        // [screenShot(searchResults[0]), screenShot(searchResults[1]), screenShot(searchResults[3])]
 
-    // const imagesMultiple = await multiple(searchResults)
-    // const imagesMultiple = screenShot(searchResults[0].link)
+        // await screenShot(searchResults[0].link).then(await screenShot(searchResults[1].link)).then(await screenShot(searchResults[2].link))
 
-    // if (imagesMultiple[0] && imagesMultiple[1] && imagesMultiple[2]) {
-    //     console.log(imagesMultiple);
-    // if (searchResults[0].length > 0 && searchResults[1].length > 0 && searchResults[2].length > 0) {
-    const arrayOfImages = []
-    console.time('promiseAll')
-    await Promise.all([screenShot(searchResults[0].link), screenSho(searchResults[1].link), screenSh(searchResults[2].link)]).then((values) => {
-        ; imagesMultiple.push(...values)
-    });
-    // imagesMultiple[0] = screenShot(searchResults[0].link)
-    // imagesMultiple[1] = screenSho(searchResults[1].link)
-    // imagesMultiple[2] = screenSh(searchResults[2].link)
-    console.timeEnd('promisAll')
 
-    // if (imagesMultiple[0].length > 1 && imagesMultiple[1].length > 1 && imagesMultiple[2].length > 1) {
-    for (let i = 0; i < 3; i++) {
-        console.time("loop")
-        // test = await screenShot(searchResults[i].link)
-        // test = test.toString('base64')
-        // test = 'data:image/png;base64,' + test
-        searchResults[i].imageCode = imagesMultiple[i]
+        // imagesMultiple[1] = await screenShot(searchResults[1].link)
+        // imagesMultiple[2] = await screenShot(searchResults[2].link)
 
-        // searchResults[i].imageCode = imagesMultiple[i]
-        arrayOfImages.push(searchResults[i])
+        // const imagesMultiple = await multiple(searchResults)
+        // const imagesMultiple = screenShot(searchResults[0].link)
+
+        // if (imagesMultiple[0] && imagesMultiple[1] && imagesMultiple[2]) {
+        //     console.log(imagesMultiple);
+        // if (searchResults[0].length > 0 && searchResults[1].length > 0 && searchResults[2].length > 0) {
+        const arrayOfImages = []
+        console.time('promiseAll')
+        await Promise.all([screenShot(searchResults[0].link), screenSho(searchResults[1].link), screenSh(searchResults[2].link)]).then((values) => {
+            ; imagesMultiple.push(...values)
+            console.timeEnd('promisAll')
+        });
+        // imagesMultiple[0] = screenShot(searchResults[0].link)
+        // imagesMultiple[1] = screenSho(searchResults[1].link)
+        // imagesMultiple[2] = screenSh(searchResults[2].link)
+
+        // console.log(imagesMultiple);
+        for (let i = 0; i < 3; i++) {
+            console.time("loop")
+            // test = await screenShot(searchResults[i].link)
+            // test = test.toString('base64')
+            // test = 'data:image/png;base64,' + test
+            searchResults[i].imageCode = imagesMultiple[i]
+
+            // searchResults[i].imageCode = imagesMultiple[i]
+            arrayOfImages.push(searchResults[i])
+        }
+
+        console.timeEnd('loop')
+
+        console.timeEnd('start')
+        res.send(arrayOfImages)
+    } catch (error) {
+        console.log(error);
+        res.send([{ imageCode: '' }])
+
     }
-    console.timeEnd('loop')
-
-    console.timeEnd('start')
-    res.send(arrayOfImages)
-
 
 
 })
